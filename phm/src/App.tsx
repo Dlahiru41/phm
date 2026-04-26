03d35f6db8a35a22895159b52f8b2f6e03f9104d
Dlahiru41 <dlahiru41@outlook.com>
2026-04-26 12:19:03 +0530

    New: change the landing page to use the login page

--- phm/src/App.tsx
+++ phm/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
 import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
 import { ChangePasswordPage } from './pages/ChangePasswordPage';
@@ -35,7 +34,7 @@ import { MohSystemOverviewReportPage } from './pages/MohSystemOverviewReportPage
 const App: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/change-password" element={<ChangePasswordPage />} />
